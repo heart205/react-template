c8265df77b5672e8d39e7c15ee023484c731b997
heart <7362469@qq.com>
2022-05-01 15:10:07 +0800

    init components

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,10 @@
  */
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-const Home = React.lazy(() => import('./pages/home/index'));
-const TestUseState = React.lazy(() => import('./pages/state/index'));
 export default () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test_use_state" element={<TestUseState />} />
       </Routes>
     </BrowserRouter>
   );
